9897ac3bd01cabf03faac44522d167bf10c816ae
Thomas Da Silva <thomas-78000@hotmail.fr>
2023-07-01 19:03:11 +0200

    mariadb script

--- srcs/requirements/mariadb/tools/script.sh
+++ srcs/requirements/mariadb/tools/script.sh
@@ -1,21 +1,33 @@
 #! /bin/bash
 
-#if [ ! -f "/var/lib/mysql/.installation_ok" ]; then
+if [ ! -f "/var/lib/mysql/.installation_ok" ]; then
 
 service mysql start
-mysql_secure_installation << STOP
+
+mysql_secure_installation << EOF
 
 Y
-root
-root
+$MYSQL_ROOT_PWD
+$MYSQL_ROOT_PWD
 Y
 n
 Y
 Y
-STOP
-service mysql stop
-touch .installation_ok
+EOF
 
-#fi
+mysql -u root -e "GRANT ALL ON *.* TO 'root'@'%' IDENTIFIED BY '$MYSQL_ROOT_PWD';
+                  CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE;
+                  CREATE USER '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PWD';
+                  GRANT ALL ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PWD';
+                  FLUSH PRIVILEGES;"
+
+sed -i 's/#port.*/port                    = 3306/' /etc/mysql/mariadb.conf.d/50-server.cnf
+sed -i 's/bind-address.*/bind-address            = 0.0.0.0/' /etc/mysql/mariadb.conf.d/50-server.cnf
+
+touch /var/lib/mysql/.installation_ok
+
+service mysql stop
+
+fi
 
 exec "$@"
